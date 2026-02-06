bd4febc9fbe7ecf2bb274f8f73053d5365b6f180
wazidrazi <wazidrazi20@gmail.com>
2026-02-06 20:05:50 +0600

    map, filter and find

---
+++ map-filter.js
@@ -0,0 +1,18 @@
+const numbers = [2, 3, 4, 5, 6, 7];
+
+let results = numbers.map(num => num * num);
+
+console.log(results);
+
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let largerNumber = nums.filter(x => x > 6);              // filter from array
+
+console.log(largerNumber);
+
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let largerNumber = nums.find(x => x > 6);              // find only one element
+
+console.log(largerNumber);
+
