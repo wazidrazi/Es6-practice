6ea57a14f1e80dd311c9d0e282863ad01a99127b
wazidrazi <wazidrazi20@gmail.com>
2026-02-07 23:44:44 +0600

    mapping

--- map-filter.js
+++ map-filter.js
@@ -16,3 +16,27 @@ let largerNumber = nums.find(x => x > 6);              // find only one element
 
 console.log(largerNumber);
 
+
+
+let ages = [12, 18, 25, 15];
+let ageLevel = ages.map(age => age >= 18 ? "Adult" : "Minor" );
+console.log (ageLevel);
+
+let marks = [35, 80, 25, 60];
+let result = marks.map(mark => mark >= 40? "✅ Pass" : "❌ Fail")
+console.log(result);
+
+
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+fruits.forEach(fruit => {
+    console.log("I Love" + fruit);
+})
+
+
+let salaries = [20000, 30000, 50000];
+salaries.forEach(salary => 
+    console.log(
+        salary + " - " + (salary >= 40000 ? "Excellent" : salary >= 25000 ? "Good" : "Low")
+    )
+);
+
